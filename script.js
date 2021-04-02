ce074d5a9ec60033112e3017f83bfdff2b3cd64b
Marcel Petersen Luciano <81695689+MarcelPLuciano@users.noreply.github.com>
2021-04-02 00:27:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var valorEmDolar = prompt("Qual valor você deseja converter em dólar???")
+
+var valorEmDolarNumero = parseFloat(valorEmDolar)
+
+var valorEmReal = valorEmDolarNumero * 5.51
+
+alert(valorEmReal.toFixed(2))
